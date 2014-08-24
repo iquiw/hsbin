84edfc725f173bd472fc642e71487b7a3228697c
Iku Iwasa <iku.iwasa@gmail.com>
2014-08-24 16:58:05 +0900

    Write hash value as hex string

--- src/Hsbin/Hash.hs
+++ src/Hsbin/Hash.hs
@@ -3,6 +3,7 @@ module Hsbin.Hash where
 import           Control.Applicative
 import           Crypto.Hash.SHA1
 import           Data.ByteString (ByteString)
+import           Data.ByteString.Base16 (decode, encode)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
 import           System.Directory
@@ -16,14 +17,14 @@ eqHash henv hscr newH = do
     let path = hsHashPath henv hscr
     b <- doesFileExist path
     if b
-        then do oldH <- B.readFile path
+        then do (oldH, _) <- decode <$> B.readFile path
                 return $ newH == oldH
         else return False
 
 writeHash :: HsbinEnv -> HsbinScript -> Hash -> IO ()
-writeHash henv hscr hash = do
+writeHash henv hscr hb = do
     let path = hsHashPath henv hscr
-    B.writeFile path hash
+    B.writeFile path $ encode hb
 
 hscrHash :: HsbinScript -> IO Hash
 hscrHash hscr = hashlazy <$> LB.readFile (hsPath hscr)
